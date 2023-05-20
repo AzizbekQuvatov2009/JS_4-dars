1e4a9a95741d41238fd89e665930cfbb7ffcbf72
AzizbekQuvatov2009 <quvatovazizbek22@gmail.com>
2023-05-20 11:08:10 +0500

    JS_4-dars kodlari
    JS_4-dars kodlari yuklandi

---
+++ JS/main1.js
@@ -0,0 +1,49 @@
+/* Function in JS (Jsda funksiyalar ishlash) */
+
+/*let jadval = 15+1;
+
+let avtobus = [jadval]
+
+
+if (avtobus.length == 4){
+    document.write("avtobusning ichida" + avtobus)
+}else {
+    document.write("marshutka ichida" + avtobus)
+}*/
+
+
+
+
+/*
+function savatchanitekshir (uzatilganMalumot = [22,false]){
+    if (uzatilganMalumot.length == 0){
+        document.write("Savatchaning ichida" + uzatilganMalumot.length + "ta ma'lumot bor");
+    }
+    else {
+        document.write("Savatchaning ichida" + uzatilganMalumot.length + "dona ma'lumot bor");
+    }
+}
+
+let container = [12,true,"matn",false];
+savatchanitekshir(container);
+*/
+
+
+function savatchanitekshir (MalumotlarBazasi = [890,true,"javob bor"]){
+    if (MalumotlarBazasi.length == 0){
+        document.write("bazaning ichida" + MalumotlarBazasi.length + "ta ma'lumot bor")
+    }
+    else {
+        document.write("bazaning ichida" + MalumotlarBazasi.length + "dona ma'lumot bor")
+    }
+}
+let bolalar = [54,345,"beshming","birnechta",true,687];
+savatchanitekshir(bolalar);
+
+
+
+
+
+
+
+
